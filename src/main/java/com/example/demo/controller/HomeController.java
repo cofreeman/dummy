61a09a948d638524c260719fa18e597a542ca3b4
cofreeman <70828640+cofreeman@users.noreply.github.com>
2022-07-06 15:05:31 +0900

    Update HomeController.java
    sd

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -3,10 +3,8 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 public class HomeController {
 
-    @RequestMapping("/home")
     public String home() {
         return "Hello, World!";
     }
